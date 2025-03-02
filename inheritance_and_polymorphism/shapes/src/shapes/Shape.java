73821f3cafce26e338c43cba101c0b5b49bd161b
Alexander Sidorenko <xarendel@gmail.com>
2025-03-02 21:25:25 +0300

    Task 2.2.3 (Instead of directly accessing variable, access methods are used)

--- inheritance_and_polymorphism/shapes/src/shapes/Shape.java
+++ inheritance_and_polymorphism/shapes/src/shapes/Shape.java
@@ -4,15 +4,15 @@ public abstract class Shape {
     private double[] initialPoint;
 
     protected Shape(double[] initialPoint) {
-        this.initialPoint = initialPoint;
+        setInitialPoint(initialPoint);
     }
 
     abstract protected double square();
     abstract protected double perimeter();
 
     protected void move(double xAxisShift, double yAxisShift) {
-        initialPoint[0] += xAxisShift;
-        initialPoint[1] += yAxisShift;
+        getInitialPoint()[0] += xAxisShift;
+        getInitialPoint()[1] += yAxisShift;
     }
 
     protected void fill() {
