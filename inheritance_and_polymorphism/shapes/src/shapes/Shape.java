f5b1c731d5f2b199b4ed240b9ff63c05bdbd1d8e
Alexander Sidorenko <xarendel@gmail.com>
2025-03-02 21:23:14 +0300

    Task 2.2.2 (Constructor, getter, and setter have been added to Shape class. Also method move has been updated)

--- inheritance_and_polymorphism/shapes/src/shapes/Shape.java
+++ inheritance_and_polymorphism/shapes/src/shapes/Shape.java
@@ -3,14 +3,27 @@ package shapes;
 public abstract class Shape {
     private double[] initialPoint;
 
+    protected Shape(double[] initialPoint) {
+        this.initialPoint = initialPoint;
+    }
+
     abstract protected double square();
     abstract protected double perimeter();
 
-    protected void move() {
-
+    protected void move(double xAxisShift, double yAxisShift) {
+        initialPoint[0] += xAxisShift;
+        initialPoint[1] += yAxisShift;
     }
 
     protected void fill() {
 
+    }
+
+    public double[] getInitialPoint() {
+        return initialPoint;
+    }
+
+    public void setInitialPoint(double[] initialPoint) {
+        this.initialPoint = initialPoint;
     }
 }
