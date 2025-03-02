acc73e8e33469c0c32397503e03b01db0d36def1
Alexander Sidorenko <xarendel@gmail.com>
2025-03-02 21:18:36 +0300

    Task 2.2.1 (Added an abstract Shape class)

---
+++ inheritance_and_polymorphism/shapes/src/shapes/Shape.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public abstract class Shape {
+    private double[] initialPoint;
+
+    abstract protected double square();
+    abstract protected double perimeter();
+
+    protected void move() {
+
+    }
+
+    protected void fill() {
+
+    }
+}
