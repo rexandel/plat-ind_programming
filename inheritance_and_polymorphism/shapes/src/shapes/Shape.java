570438e7627ef9d082ba235ec1ac8aa9d974e8a5
Alexander Sidorenko <xarendel@gmail.com>
2025-03-02 22:12:10 +0300

    Task 2.2.5 (Added fields responsible for the color of lines and fills)

--- inheritance_and_polymorphism/shapes/src/shapes/Shape.java
+++ inheritance_and_polymorphism/shapes/src/shapes/Shape.java
@@ -1,29 +1,47 @@
 package shapes;
 
+import java.awt.*;
+
 public abstract class Shape {
     protected double[] initialPoint;
-
-    protected Shape(double[] initialPoint) {
-        setInitialPoint(initialPoint);
-    }
+    protected Color lineColor;
+    protected Color fillColor;
 
     abstract protected double square();
     abstract protected double perimeter();
+
+    protected Shape(double[] initialPoint) {
+        setInitialPoint(initialPoint);
+        setLineColor(Color.white);
+        setFillColor(Color.black);
+    }
 
     protected void move(double xAxisShift, double yAxisShift) {
         getInitialPoint()[0] += xAxisShift;
         getInitialPoint()[1] += yAxisShift;
     }
 
-    protected void fill() {
-
-    }
-
     public double[] getInitialPoint() {
         return initialPoint;
     }
 
-    public void setInitialPoint(double[] initialPoint) {
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+
+    private void setInitialPoint(double[] initialPoint) {
         this.initialPoint = initialPoint;
+    }
+
+    private void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    private void setFillColor(Color fillColor) {
+        this.fillColor = fillColor;
     }
 }
