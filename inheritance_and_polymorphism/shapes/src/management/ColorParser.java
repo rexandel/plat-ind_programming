24275c66306a553d938d6e7bfa2fec849529a049
Alexander Sidorenko <xarendel@gmail.com>
2025-03-03 12:07:50 +0300

    Task 2.2.10 (Updated ColorParser class: equals method is used)

--- inheritance_and_polymorphism/shapes/src/management/ColorParser.java
+++ inheritance_and_polymorphism/shapes/src/management/ColorParser.java
@@ -20,23 +20,23 @@ public class ColorParser {
     }
 
     private static String getColorName(int[] colorArray) {
-        if (isEqual(colorArray, new int[] {255, 255, 255})) {
+        if (Arrays.equals(colorArray, new int[] {255, 255, 255})) {
             return "White";
-        } else if (isEqual(colorArray, new int[] {0, 0, 0})) {
+        } else if (Arrays.equals(colorArray, new int[] {0, 0, 0})) {
             return "Black";
-        } else if (isEqual(colorArray, new int[] {160, 163, 160})) {
+        } else if (Arrays.equals(colorArray, new int[] {128, 128, 128})) {
             return "Gray";
-        } else if (isEqual(colorArray, new int[] {255, 0, 0})) {
+        } else if (Arrays.equals(colorArray, new int[] {255, 0, 0})) {
             return "Red";
-        } else if (isEqual(colorArray, new int[] {0, 0, 255})) {
+        } else if (Arrays.equals(colorArray, new int[] {0, 0, 255})) {
             return "Blue";
-        } else if (isEqual(colorArray, new int[] {0, 255, 0})) {
+        } else if (Arrays.equals(colorArray, new int[] {0, 255, 0})) {
             return "Green";
-        } else if (isEqual(colorArray, new int[] {255, 255, 0})) {
+        } else if (Arrays.equals(colorArray, new int[] {255, 255, 0})) {
             return "Yellow";
-        } else if (isEqual(colorArray, new int[] {255, 0, 255})) {
+        } else if (Arrays.equals(colorArray, new int[] {255, 0, 255})) {
             return "Magenta";
-        } else if (isEqual(colorArray, new int[] {136, 216, 192})) {
+        } else if (Arrays.equals(colorArray, new int[] {136, 216, 192})) {
             return "Pearl Aqua";
         } else {
             return "Unknown";
@@ -52,17 +52,5 @@ public class ColorParser {
         colorArray[3] = color.getAlpha();
 
         return colorArray;
-    }
-
-    private static boolean isEqual(int[] firstColorArray, int[] secondColorArray) {
-        if (firstColorArray.length != secondColorArray.length) {
-            return false;
-        }
-        for (int i = 0; i < firstColorArray.length; i++) {
-            if (firstColorArray[i] != secondColorArray[i]) {
-                return false;
-            }
-        }
-        return true;
     }
 }
