abff9530e095e59d091acc4951be81cb257afd4c
Alexander Sidorenko <xarendel@gmail.com>
2025-03-09 21:33:31 +0300

    Task 2.2.17 (Rectangle class added)

---
+++ inheritance_and_polymorphism/shapes/src/shapes/Rectangle.java
@@ -0,0 +1,105 @@
+package shapes;
+
+import java.awt.Color;
+import java.util.Arrays;
+
+public class Rectangle extends Shape {
+    private double[] sidePoint;
+    private double length;
+    private double height;
+
+    public Rectangle(double[] initialPoint, double[] sidePoint) {
+        super(initialPoint);
+        setSidePoint(sidePoint);
+        setLength(Math.max(initialPoint[0], sidePoint[0]) - Math.min(initialPoint[0], sidePoint[0]));
+        setHeight(Math.max(initialPoint[1], sidePoint[1]) - Math.min(initialPoint[1], sidePoint[1]));
+    }
+
+    public Rectangle(double[] initialPoint, double[] sidePoint, Color lineColor) {
+        super(initialPoint, lineColor);
+        setSidePoint(sidePoint);
+        setLength(Math.max(initialPoint[0], sidePoint[0]) - Math.min(initialPoint[0], sidePoint[0]));
+        setHeight(Math.max(initialPoint[1], sidePoint[1]) - Math.min(initialPoint[1], sidePoint[1]));
+    }
+
+    @Override
+    protected void move(double xAxisShift, double yAxisShift) {
+        super.move(xAxisShift, yAxisShift);
+
+        double[] currentPoint = getSidePoint();
+
+        double newX = currentPoint[0] + xAxisShift;
+        double newY = currentPoint[1] + yAxisShift;
+
+        setSidePoint(new double[]{newX, newY});
+    }
+
+    public double square() {
+        return length * height;
+    }
+
+    public double perimeter() {
+        return 2 * (length + height);
+    }
+
+    private void setSidePoint(double[] sidePoint) {
+        if (sidePoint == null) {
+            throw new IllegalArgumentException("Array with coordinates of side point cannot be null.");
+        }
+
+        if (sidePoint.length != 2) {
+            throw new IllegalArgumentException("Array with coordinates of side point must contain exactly two elements.");
+        }
+
+        if (Arrays.equals(sidePoint, getInitialPoint())) {
+            throw new IllegalArgumentException("Side point cannot match initial point.");
+        }
+
+        this.sidePoint = sidePoint;
+    }
+
+    private void setLength(double length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("Height cannot be less than zero.");
+        }
+
+        if (length == 0) {
+            throw new IllegalArgumentException("Length cannot be zero.");
+        }
+
+        this.length = length;
+    }
+
+    private void setHeight(Double height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("Height cannot be less than zero.");
+        }
+
+        if (height == 0) {
+            throw new IllegalArgumentException("Height cannot be zero.");
+        }
+
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return
+                super.toString() + "\n" +
+                "Side Point: " + Arrays.toString(getSidePoint()) + "\n" +
+                "Length: " + getLength() + "\n" +
+                "Height: " + getHeight();
+    }
+
+    public double[] getSidePoint() {
+        return sidePoint;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
