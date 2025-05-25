76803b8bc45942977bdba74a32d9f85cafee0b8e
Alexander Sidorenko <xarendel@gmail.com>
2025-05-25 17:31:08 +0300

    Task 4 (Updated example of windowed app: added label)

--- graphical_user_interface/examples/Main.java
+++ graphical_user_interface/examples/Main.java
@@ -3,8 +3,10 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My Application");
+        JLabel label = new JLabel("Hello world!");
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(label);
 
         frame.setLocationRelativeTo(null);
 
