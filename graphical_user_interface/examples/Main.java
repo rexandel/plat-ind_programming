d0dce1b813c8f5e0b75093cf0e6b406829dfd174
Alexander Sidorenko <xarendel@gmail.com>
2025-05-25 17:36:24 +0300

    Task 4.1 (Updated example of windowed app: added buttons)

--- graphical_user_interface/examples/Main.java
+++ graphical_user_interface/examples/Main.java
@@ -7,12 +7,20 @@ public class Main {
         JLabel secondLabel = new JLabel("Hello, I'm here!");
         JLabel thirdLabel = new JLabel("Hello there...");
 
+        JButton firstButton = new JButton("Press me!");
+        JButton secondButton = new JButton("Don't touch other buttons...");
+        JButton thirdButton = new JButton("Hello, click on me plz");
+
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.add(firstLabel);
         frame.add(secondLabel);
         frame.add(thirdLabel);
+
+        frame.add(firstButton);
+        frame.add(secondButton);
+        frame.add(thirdButton);
 
         frame.setLocationRelativeTo(null);
 
