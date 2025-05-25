6e26ad3789107f56c6b96e1c7aa512a2870720bc
Alexander Sidorenko <xarendel@gmail.com>
2025-05-25 17:29:25 +0300

    Task 4 (Added example of windowed app)

---
+++ graphical_user_interface/examples/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Application");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
