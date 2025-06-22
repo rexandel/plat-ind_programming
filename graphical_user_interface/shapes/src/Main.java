0dbbc3f59d295eafe46d63556521a0f622e64fb6
Alexander Sidorenko <xarendel@gmail.com>
2025-06-23 00:45:21 +0300

    Task 4.1 (Deleted UserInterface class)

--- graphical_user_interface/shapes/src/Main.java
+++ graphical_user_interface/shapes/src/Main.java
@@ -1,13 +1,8 @@
-import management.UserInteraction;
-
 import shapes.Shape;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Shape> shapes = new ArrayList<Shape>();
-        UserInteraction ui = new UserInteraction(shapes);
 
-        while (true) ui.inputMainChoice();
     }
 }
